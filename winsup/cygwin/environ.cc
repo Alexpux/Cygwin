ea82e47868bbd09f4280cd33545b8f95c1862ed6
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-02-15 15:43:04 +0000

    Instead of swallowing the TZ environment variable, pass it on as MSYS_TZ
    It is true that pure Windows programs cannot handle the TZ variable
    well. However, the same is not true of Git, which handles the timezone
    environment variable quite well. Let's give it a chance by passing on
    the TZ environment variable as MSYS2_TZ.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- winsup/cygwin/environ.cc
+++ winsup/cygwin/environ.cc
@@ -1135,7 +1135,12 @@ build_env (const char * const *envp, PWCHAR &envblock, int &envc,
       bool calc_tl = !no_envblock;
       /* Don't pass timezone environment to non-msys applications */
       if (!keep_posix && ascii_strncasematch(*srcp, "TZ=", 3))
-        goto next1;
+        {
+          *dstp = (char *) cmalloc (HEAP_1_STR, strlen (*srcp) + 7);
+          strcpy (*dstp, "MSYS2_");
+          strcpy (*dstp + 6, *srcp);
+          goto next0;
+        }
       /* Look for entries that require special attention */
       for (unsigned i = 0; i < SPENVS_SIZE; i++)
 	if (!saw_spenv[i] && (*dstp = spenvs[i].retrieve (no_envblock, *srcp)))
