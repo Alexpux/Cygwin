ef8d5cf3bfe3cf16000a8d74c55341b8346e879f
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-09 17:50:06 +0100

    Tell non-MSys2 executables whether they are connected to a terminal
    By setting the MSYS_TTY_HANDLES environment variable to a list of
    Win32 handles corresponding to terminals connected to
    stdin/stdout/stderr, we help non-MSys2 executables (which cannot simply
    use MSys2's isatty()) to tell whether their standard file descriptors
    are redirected or not.
    
    To make things as convenient as possible, the list is delimited by
    spaces on both sides of the elements so that to find out whether stdin
    is actually a terminal, one just needs to look for the needle
    " <handle> " in $MSYS_TTY_HANDLES (where <handle> is the decimal
    representation of GetStdHandle(STD_INPUT_HANDLE)).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- winsup/cygwin/cygheap.h
+++ winsup/cygwin/cygheap.h
@@ -138,6 +138,7 @@ public:
   const char *env_domain (const char *, size_t);
   const char *env_name (const char *, size_t);
   const char *env_systemroot (const char *, size_t);
+  const char *env_tty_handles (const char *, size_t);
 
   const char *logsrv ()
   {
