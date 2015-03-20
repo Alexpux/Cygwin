d5685f8ae78d76d5747f97c656192a4a2efc8a92
Alexpux <alexey.pawlow@gmail.com>
2015-03-20 14:09:29 +0300

    Fix converting argument looks like -DVAR=str1;c:/path1;str2;c:/path2

--- winsup/cygwin/msys2_path_conv.cc
+++ winsup/cygwin/msys2_path_conv.cc
@@ -414,6 +414,9 @@ path_type find_path_start_and_type(const char** src, int recurse, const char* en
       if (isalpha(*it)) {
         it += 1;
         starts_with_minus_alpha = 1;
+        if (memchr(it, ';', end - it)) {
+        	return WINDOWS_PATH_LIST;
+        }
       }
     }
 
