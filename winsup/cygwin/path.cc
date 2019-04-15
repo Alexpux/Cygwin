6364e4c72a4c0d31df08f9a2f115612056a6dfd2
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-04-15 13:26:43 +0200

    fixup! arg_heuristic(): be better prepared to handle path lists
    This patch was rolled up into a combined on in MSYS2; Drop it.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- winsup/cygwin/path.cc
+++ winsup/cygwin/path.cc
@@ -3630,9 +3630,7 @@ arg_heuristic_with_exclusions (char const * const arg, char const * exclusions, 
       exclusions += strlen (exclusions) + 1;
     }
 
-  // Leave enough room for at least 16 path elements; we might be converting
-  // a path list.
-  size_t stack_len = arglen + 16 * MAX_PATH;
+  size_t stack_len = arglen + MAX_PATH;
   char * stack_path = (char *)malloc (stack_len);
   if (!stack_path)
     {
